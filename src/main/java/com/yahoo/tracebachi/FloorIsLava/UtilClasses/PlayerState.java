c806294213b22bb6aea88917885efbf951385d57
Trace <tracebachi@yahoo.com>
2015-11-27 18:21:36 -0800

    Corrected preamble in source files

--- src/main/java/com/yahoo/tracebachi/FloorIsLava/UtilClasses/PlayerState.java
+++ src/main/java/com/yahoo/tracebachi/FloorIsLava/UtilClasses/PlayerState.java
@@ -1,18 +1,18 @@
-/* FloorIsLava Minigame for Multiplayer Minecraft
- * Copyright (C) 2015 Trace Bachi (tracebachi@yahoo.com)
+/*
+ * This file is part of FloorIsLava.
  *
- * This program is free software: you can redistribute it and/or modify
+ * FloorIsLava is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * This program is distributed in the hope that it will be useful,
+ * FloorIsLava is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * along with FloorIsLava.  If not, see <http://www.gnu.org/licenses/>.
  */
 package com.yahoo.tracebachi.FloorIsLava.UtilClasses;
 
