fe754a2c754e1986794d623426829b6475cb54bb
Jeremy Lugo <jeremylugo110997@gmail.com>
2016-09-12 23:16:15 -0400

    Fix offhand slot bugs.

--- src/main/java/com/gmail/tracebachi/FloorIsLava/Utils/PlayerState.java
+++ src/main/java/com/gmail/tracebachi/FloorIsLava/Utils/PlayerState.java
@@ -29,7 +29,8 @@ public class PlayerState
     private boolean flyAllowed;
     private Location location;
     private ItemStack[] armor;
-    private ItemStack[] content;
+    private ItemStack[] storage;
+    private ItemStack[] extra;
     private GameMode gameMode;
 
     public void save(Player target)
@@ -41,14 +42,16 @@ public class PlayerState
 
         location = target.getLocation();
         armor = target.getInventory().getArmorContents();
-        content = target.getInventory().getStorageContents();
+        storage = target.getInventory().getStorageContents();
+        extra = target.getInventory().getExtraContents();
     }
 
     public void restoreInventory(Player target)
     {
         target.setHealth(20.0);
         target.getInventory().setArmorContents(armor);
-        target.getInventory().setStorageContents(content);
+        target.getInventory().setStorageContents(storage);
+        target.getInventory().setExtraContents(extra);
     }
 
     public void restoreLocation(Player target)
